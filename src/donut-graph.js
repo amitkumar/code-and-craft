2b097e00cf514b0d7e411a2b020cb4c8ea7a6c35
Amit Kumar <email@amitkumar.com>
2016-10-22 02:34:35 -0400

    moving to manual animations/easing rather than d3.transition. Currently exhibiting a "zip around" donut animation

--- src/donut-graph.js
+++ src/donut-graph.js
@@ -9,47 +9,41 @@ export default class {
 		this.radius = this.width / 2;
 		this.thickness = thickness;
 
-		// Store the displayed angles in _current.
-		// Then, interpolate from _current to the new angles.
-		// During the transition, _current is updated in-place by d3.interpolate.
-		function arcTween(a) {
-			var i = d3.interpolate(this._current, a);
-			this._current = i(0);
-			return function(t) {
-				return arc(i(t));
-			};
-		}
-
-		this.draw = function(){
-			console.log('draw');
-			g.datum(this.timerData);
-			path.data(pie); // compute the new angles
-		    path.transition().duration(1000).attrTween("d", arcTween); // redraw the arcs
-		}
-		this.start = function(){
-			this.timer.start();
-		}
-
-
-		this.timer = new Timer(duration, 1001, ::this.draw, ::this.draw);
-		
-		const data = this.timerData;
-		const colorScale = d3.scaleLinear().domain([0, data.length - 1]).range([.8, .2]);
-		const color = function(index){
-			return d3.interpolateInferno(colorScale(index));
-		};
-		
 		const pie = d3.pie().sort(null);
 
 		const arc = d3.arc()
 		.innerRadius(this.radius - thickness)
 		.outerRadius(this.radius);
 
+
+		this.draw = function(){
+			g.datum(this.timerData);
+			path.data(pie)
+				.attr("d", arcEased);
+		}
+		this.start = function(){
+			this.timer.start();
+		}
+
+		this.timer = new Timer(duration, ::this.draw, ::this.draw);
+		
+		const arcEased = function(d, i){
+			var interpolate = d3.interpolate(this._current, d);
+			return arc(interpolate(d3.easeCubic(oneSecondScaleIn(d.value))));
+		}
+
+		const data = this.timerData;
+		const oneSecondScaleIn = d3.scaleLinear().domain([0, 1000]).range([0, 1]);
+		const colorScale = d3.scaleLinear().domain([0, data.length - 1]).range([.8, .2]);
+		const color = function(index){
+			return d3.interpolateInferno(colorScale(index));
+		};
+		
 		const g = this.svg
 		.append("g")
 		.attr("transform", "translate(" + this.width / 2 + "," + this.width / 2 + ")");
 
-		let path = g.datum(data).selectAll("path")
+		const path = g.datum(data).selectAll("path")
 		.data(pie)
 		.enter().append("path")
 		.attr("fill", function(d, i) { return color(i); })
@@ -76,13 +70,14 @@ export default class {
 	static generateTimerData(duration, elapsed){
 		console.log('duration, elapsed', duration, elapsed);
 		const numIndices = Math.floor(duration / 1000);
-		const numIndicesElapsed = Math.ceil(elapsed / 1000);
+		const numIndicesCompleted = Math.floor(elapsed / 1000);
 		let result = Array(numIndices + 1).fill(0);
-		for(let i = 0; i < numIndicesElapsed; i++){
+		for(let i = 0; i < numIndicesCompleted; i++){
 			result[i] = 1000;
 		}
-		result[result.length - 1] = (numIndices - numIndicesElapsed) * 1000;
-		console.log('numIndices', numIndices, 'numIndicesElapsed', numIndicesElapsed);
+		result[numIndicesCompleted] = elapsed % 1000;
+		result[result.length - 1] = duration - elapsed; //(numIndices - numIndicesElapsed) * 1000;
+		console.log('numIndices', numIndices, 'numIndicesCompleted', numIndicesCompleted);
 		console.log(result);
 		return result;
 	}
