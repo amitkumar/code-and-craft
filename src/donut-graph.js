69c3e4f687ab429757e35102f21cfeeafb71a5fa
Amit Kumar <email@amitkumar.com>
2016-10-22 16:31:34 -0400

    refactoring graph class for clearer definition of settings

--- src/donut-graph.js
+++ src/donut-graph.js
@@ -2,17 +2,19 @@ import * as d3 from 'd3';
 import Timer from './timer';
 
 export default class {
-	constructor(id, svg, duration, width, thickness){
-		this.id = id;
-		this.svg = svg;
-		this.width = width;
+	constructor(settings){
+		this.id = settings.id;
+		this.svg = settings.svg;
+		this.width = settings.width;
 		this.radius = this.width / 2;
-		this.thickness = thickness;
+		this.thickness = settings.thickness;
+
+		const svgRectangle = this.svg.node().getBoundingClientRect();
 
 		const pie = d3.pie().sort(null);
 
 		const arc = d3.arc()
-		.innerRadius(this.radius - thickness)
+		.innerRadius(this.radius - this.thickness)
 		.outerRadius(this.radius);
 
 
@@ -25,7 +27,7 @@ export default class {
 			this.timer.start();
 		}
 
-		this.timer = new Timer(duration, ::this.draw, ::this.draw);
+		this.timer = new Timer(settings.duration, ::this.draw, ::this.draw);
 		
 		const arcEased = function(d, i){
 			var interpolate = d3.interpolate(this._current, d);
@@ -41,7 +43,7 @@ export default class {
 		
 		const g = this.svg
 		.append("g")
-		.attr("transform", "translate(" + this.width / 2 + "," + this.width / 2 + ")");
+		.attr("transform", "translate(" + svgRectangle.width / 2 + "," + svgRectangle.height / 2 + ")");
 
 		const path = g.datum(data).selectAll("path")
 		.data(pie)
@@ -49,13 +51,6 @@ export default class {
 		.attr("fill", function(d, i) { return color(i); })
 		.attr("d", arc)
 	    .each(function(d) { this._current = d; }); // store the initial angles
-	}
-
-	get elapsed(){
-		if (!this.running){
-			return 0;
-		}
-		return Date.now() - this.startedAt;
 	}
 
 	get timerData(){
@@ -68,7 +63,7 @@ export default class {
 	 * set to 1000, with last item in array holding remaining duration.
 	 */
 	static generateTimerData(duration, elapsed){
-		console.log('duration, elapsed', duration, elapsed);
+		// console.log('duration, elapsed', duration, elapsed);
 		const numIndices = Math.floor(duration / 1000);
 		let numIndicesCompleted = Math.floor(elapsed / 1000);
 		if (numIndicesCompleted > numIndices){
@@ -81,8 +76,8 @@ export default class {
 		result[numIndicesCompleted] = elapsed % 1000;
 
 		result[result.length - 1] = duration - elapsed; //(numIndices - numIndicesElapsed) * 1000;
-		console.log('numIndices', numIndices, 'numIndicesCompleted', numIndicesCompleted);
-		console.log(result);
+		// console.log('numIndices', numIndices, 'numIndicesCompleted', numIndicesCompleted);
+		// console.log(result);
 		return result;
 	}
 }
--- src/main.js
+++ src/main.js
@@ -14,63 +14,22 @@ jquery(function(){
 	// 90 minutes, converted to milliseconds
 	const globalTimer = new Timer(90 * 60 * 1000);
 
-
 	const craftTimer = new Timer(15 * 1000);
 
-	var $craftTimer = jquery('#craft-timer'),
+	const $craftTimer = jquery('#craft-timer'),
 		$startGlobalTimer = jquery('#btn-start-global-timer'),
 		craftTimerWidth = $craftTimer.width(),
 		craftTimerHeight = $craftTimer.height();
 
-	var $soundToggle = jquery('#btn-toggle-sound');
+	const $soundToggle = jquery('#btn-toggle-sound');
 	
-	var width = 100,
-	height = 100,
-	radius = Math.min(width, height) / 2;
-	
-	var data = DonutGraph.generateTimerData(craftTimer.duration, craftTimer.elapsed);
-	
-	var colorScale = d3.scaleLinear().domain([0, data.length - 1]).range([.8, .2]);
-	var color = function(index){
-		console.log('index', index, colorScale(index));
-		return d3.interpolateInferno(colorScale(index));
-	};
-	
-	var pie = d3.pie().sort(null);
+	const svg = d3.select('#craft-timer');
+	const svgRectangle = svg.node().getBoundingClientRect();
 
-	var arc = d3.arc()
-	.innerRadius(radius - 500)
-	.outerRadius(radius);
-
-	var svg = d3.select('#craft-timer');
-
-	var g = svg	
-	.append("g")
-	.attr("transform", "translate(" + craftTimerWidth / 2 + "," + craftTimerHeight / 2 + ")");
-
-	var path = g.datum(data).selectAll("path")
-	.data(pie)
-	.enter().append("path")
-	.attr("fill", function(d, i) { return color(i); })
-	.attr("d", arc)
-    .each(function(d) { this._current = d; }); // store the initial angles
-
-    // Store the displayed angles in _current.
-	// Then, interpolate from _current to the new angles.
-	// During the transition, _current is updated in-place by d3.interpolate.
-	function arcTween(a) {
-		var i = d3.interpolate(this._current, a);
-		this._current = i(0);
-		return function(t) {
-			return arc(i(t));
-		};
-	}
-
-    
 	$startGlobalTimer.on('click', function(){
 		setTimeout(function(){
 			startGlobalTimer();
-			startCraftTimer();
+			// startCraftTimer();
 		}, 0);
 	});
 	$soundToggle.on('click', function(){
@@ -86,41 +45,39 @@ jquery(function(){
 	function startGlobalTimer(){
 		globalTimer.start();
 	}
-	function drawCraftTimer(){
-		var data = DonutGraph.generateTimerData(craftTimer.duration, craftTimer.elapsed);
-		g.datum(data);
-		path = path.data(pie); // compute the new angles
-	    path.transition().duration(1000).attrTween("d", arcTween); // redraw the arcs
-	}
-	function startCraftTimer(){
-		craftTimer.start();
-	}
-	function stopCraftTimer(){
-		craftTimer.stop();
-		CraftAudio.end();
-		drawCraftTimer();
-	}
-
-
-	const threeMinuteDonut = new DonutGraph('three-minute', svg, 10 * 1000, 500, 20);
-	threeMinuteDonut.start();
-
-
-    
- //    function everySecond() {
-	// 	if (craftTimer.running){
-	// 		if (craftTimer.elapsed === 0){
-	// 			CraftAudio.start();
-	// 		}	
-	// 		craftTimer.elapsed = craftTimer.elapsed + 1000;
-	// 		drawCraftTimer();
-	// 	}
-
-	// 	if (craftTimer.elapsed > craftTimer.duration){
- //    		stopCraftTimer();
- //    	}
-
- //    	threeMinuteDonut.draw();
+	// function drawCraftTimer(){
+	// 	var data = DonutGraph.generateTimerData(craftTimer.duration, craftTimer.elapsed);
+	// 	g.datum(data);
+	// 	path = path.data(pie); // compute the new angles
+	//     path.transition().duration(1000).attrTween("d", arcTween); // redraw the arcs
 	// }
+	// function startCraftTimer(){
+	// 	craftTimer.start();
+	// }
+	// function stopCraftTimer(){
+	// 	craftTimer.stop();
+	// 	CraftAudio.end();
+	// 	drawCraftTimer();
+	// }
+
+
+
+	// const craftTimerGraph = new DonutGraph({
+	// 	id : 'craft-10-sec',
+	// 	svg: svg, 
+	// 	duration: 10 * 1000, 
+	// 	width: 100, 
+	// 	thickness: Math.min(svgRectangle.width, svgRectangle.height)/2 + 50
+	// });
+	// Timer.start();
+
+	const workTimerGraph = new DonutGraph({
+		id : 'three-minute',
+		svg: svg, 
+		duration: 10 * 1000, 
+		width: Math.min(svgRectangle.width, svgRectangle.height) - 50, 
+		thickness: 100
+	});
+	workTimerGraph.start();
 
 });
