681a39875e1662101733cd7e4c56090114f26339
Amit Kumar <email@amitkumar.com>
2017-03-05 17:20:48 -0500

    moving GLC, renaming some routes

--- app.js
+++ app.js
@@ -1,49 +1,90 @@
-/**
- * Require Browsersync along with webpack and middleware for it
- */
-var browserSync          = require('browser-sync').create();
-var webpack              = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var stripAnsi            = require('strip-ansi');
+// /**
+//  * Require Browsersync along with webpack and middleware for it
+//  */
+// var browserSync          = require('browser-sync').create();
+// var webpack              = require('webpack');
+// var webpackDevMiddleware = require('webpack-dev-middleware');
+// var stripAnsi            = require('strip-ansi');
 
-/**
- * Require ./webpack.config.js and make a bundler from it
- */
-var webpackConfig = require('./webpack.config');
-var bundler       = webpack(webpackConfig);
+// /**
+//  * Require ./webpack.config.js and make a bundler from it
+//  */
+// var webpackConfig = require('./webpack.config');
+// var bundler       = webpack(webpackConfig);
 
-/**
- * Reload all devices when bundle is complete
- * or send a fullscreen error message to the browser instead
- */
-bundler.plugin('done', function (stats) {
-    if (stats.hasErrors() || stats.hasWarnings()) {
-        return browserSync.sockets.emit('fullscreen:message', {
-            title: "Webpack Error:",
-            body:  stripAnsi(stats.toString()),
-            timeout: 100000
-        });
-    }
-    browserSync.reload();
-});
+// /**
+//  * Reload all devices when bundle is complete
+//  * or send a fullscreen error message to the browser instead
+//  */
+// bundler.plugin('done', function (stats) {
+//     if (stats.hasErrors() || stats.hasWarnings()) {
+//         return browserSync.sockets.emit('fullscreen:message', {
+//             title: "Webpack Error:",
+//             body:  stripAnsi(stats.toString()),
+//             timeout: 100000
+//         });
+//     }
+//     browserSync.reload();
+// });
 
-/**
- * Run Browsersync and use middleware for Hot Module Replacement
- */
-browserSync.init({
-    server: 'public',
-    open: false,
-    logFileChanges: false,
-    notify: false,
-    middleware: [
-        webpackDevMiddleware(bundler, {
-            publicPath: webpackConfig.output.publicPath,
-            stats: {colors: true}
-        })
-    ],
-    plugins: ['bs-fullscreen-message'],
-    files: [
-        'public/css/*.css',
-        'public/*.html'
-    ]
-});
+// /**
+//  * Run Browsersync and use middleware for Hot Module Replacement
+//  */
+// browserSync.init({
+//     server: 'public',
+//     open: false,
+//     logFileChanges: false,
+//     notify: false,
+//     middleware: [
+//         webpackDevMiddleware(bundler, {
+//             publicPath: webpackConfig.output.publicPath,
+//             stats: {colors: true}
+//         })
+//     ],
+//     plugins: ['bs-fullscreen-message'],
+//     files: [
+//         'public/css/*.css',
+//         'public/*.html'
+//     ]
+// });
+
+
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const path = require('path')
+const fs = require('fs')
+const exec = require('child_process').exec
+const {changeRepo, promiseGitInit, promiseGitCommit, promiseDir} = require('./lib/fs-git-hooks')
+
+app.use(require('cookie-parser')())
+app.use(require('body-parser').urlencoded({ extended: true }))
+app.use(require('body-parser').json())
+app.use(express.static(path.join(__dirname,'/public/')))
+
+app.get('/', (req,res) => {
+  res.sendFile('index.html')
+})
+
+app.post('/new-user', (req,res) => {
+  res.cookie('username',req.body.username)
+  res.redirect('/glc')
+})
+
+app.get('/glc', (req,res) => {
+    res.sendFile(path.join(__dirname,'/public/glc.html'))
+})
+
+app.post('/compile', (req,res) => {
+  console.log(req.cookies.username)
+  let thisCompilePath = path.join(changeRepo, req.cookies.username)
+  // console.log(req.body.source)
+  promiseDir(thisCompilePath)
+  .then(promiseGitInit)
+  .then(()=>promiseGitCommit(thisCompilePath, req.body, `autocommit from ${req.cookies.username}`))
+  .then(status => res.send(status))
+  .catch(problem => res.send(problem))
+})
+
+server.listen(3000)
+console.log('server listening on 3000')
