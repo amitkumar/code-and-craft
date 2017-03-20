3aa8e4a88662272eb4ddf2a0adbd5aa3f2af6382
Colten Jackson <Colten Jackson>
2017-03-19 20:02:25 -0400

    Socket handles order of connections. dashboard update.

---
+++ dist/js/dashboardSockets.js
@@ -0,0 +1,16 @@
+const socket = io()
+socket.on('infoArray', data => {
+  console.log(data)
+  data.forEach( user => {
+    let thisVideoBox = document.createElement('div')
+    let thisVideo = document.createElement('video')
+    let thisLabel = document.createElement('h3')
+    thisLabel.textContent = user.username
+    thisVideo.setAttribute('src', '/' + user.path)
+    thisVideo.setAttribute('autoplay', true)
+    thisVideo.setAttribute('loop', true)
+    thisVideoBox.appendChild(thisLabel)
+    thisVideoBox.appendChild(thisVideo)
+    document.getElementsByTagName('grid')[0].appendChild(thisVideoBox)
+  })
+})
