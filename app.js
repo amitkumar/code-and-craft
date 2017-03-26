9886f5a168af54b7b2fdbfb5ef53160261756a33
Amit Kumar <email@amitkumar.com>
2017-03-25 23:16:10 -0400

    using namespaced sockets to separate GLC interface sessions from dashboard sessions, switching to reading from session store instead of creating separate data structure for reading users

--- app.js
+++ app.js
@@ -94,26 +94,31 @@ app.get('/', (req, res) => {
 });
 
 app.get('/grapevine/dashboard', (req, res) => {
-    req.session.username = 'dashboard'
-    res.render('grapevine/dashboard', {
-        user : req.session.username
-    });
+    res.render('grapevine/dashboard', {});
 });
 
 app.get('/grapevine/glc', (req,res) => {
-    res.render('grapevine/glc', {
-        user : req.session.username
-    });
+    if (!req.session.username){
+        return res.redirect('/');
+    } else {
+        res.render('grapevine/glc', {
+            user : req.session.username
+        });    
+    }
 });
 
 app.post('/grapevine/new-user', (req,res) => {
-  req.session.username = req.body.username
-  res.redirect('/grapevine/glc')
+    if (!req.body.username){
+        return res.redirect('/');
+    } else {
+        req.session.username = req.body.username
+        res.redirect('/grapevine/glc')
+    }
 });
 
 
 app.get('/glc', (req,res) => {
-  res.sendFile(path.join(__dirname,'/dist/glc.html'))
+    res.sendFile(path.join(__dirname,'/dist/glc.html'))
 })
 
 //later, the upload function will also grab the source of the codemirror and commit it.
@@ -121,7 +126,7 @@ app.post('/commit', (req,res) => {
   console.log(req.session)
   console.log(req.session.username)
   let thisCompilePath = path.join(changeRepo, req.session.username)
-  // console.log(req.body.source)
+
   promiseDir(thisCompilePath)
   .then(promiseGitInit)
   .then(()=>promiseGitCommit(thisCompilePath, req.body, `autocommit from ${req.session.username}`))
@@ -129,13 +134,7 @@ app.post('/commit', (req,res) => {
   .catch(problem => res.send(problem))
 })
 
-//session 
 
-//dashboard 
-
-//update src attribute of sessions object in session array per user. 
-
-//
 app.post('/upload', (req,res) => {
   console.log(req.session.username)
   let videoFile = req.files.video;
@@ -149,24 +148,18 @@ app.post('/upload', (req,res) => {
                 reject(err)
                 res.status(500).send(err);
             } else {
-                // req.session.path = clientSrc
-                //make sure obj saved in the usernameToSession object has the path property assigned to it. Req.session is supposed to allow that.
-                Object.assign(usernameToSession[req.session.username], {path: clientSrc})
-                updateDashboard()
+                req.session.latestVideoPath = clientSrc;
+                broadcastVideos()
                 res.send('File uploaded.');
                 resolve(thisFileName + 'written successfully')
             }
         });
     })
-    // .then(message =>{
-    //     console.log(message)
-    //     Object.assign(usernameToSession[req.session.username], {path: '\\' + clientSrc})
-    //     updateDashboard()
-    // })
   )
   .catch(console.log.bind(console))
 
 })
+
 
 var server_port = process.env.PORT || 3000;
 var server_ip_address = process.env.IP || '127.0.0.1';
@@ -175,33 +168,31 @@ var server_ip_address = process.env.IP || '127.0.0.1';
 server.listen(server_port, server_ip_address);
 console.log(`Server listening on ${server_ip_address}:${server_port}`);
 
-let activeClients = []
-let usernameToSession = {}
+const dashboardSockets = io.of('/dashboard');
 
 io.on('connection', socket => { 
     let {username} = socket.handshake.session
     if(username != undefined && username != 'dashboard'){
-        usernameToSession[username] = socket.handshake.session
-        activeClients.push(username)
         console.log(username, 'joined.')
-        console.log(activeClients)
+        
         socket.on('disconnect', () => {
-            activeClients = activeClients.filter(user => user != username) //pop off the user thats not there anymore //screws up if two people have the same username. like undefined.
-            updateDashboard()
-            console.log(username, 'went away')
-            console.log(activeClients)
-        })
-    } 
-})
+            console.log(username, 'went away');
+            broadcastVideos();
+        });
+    }
+});
 
-function updateDashboard(){
-    infoArray = activeClients.map(username => {
-        seshcopy = Object.assign({},usernameToSession[username])
-        delete seshcopy.cookie
-        return seshcopy
-    })//now there's an array of objects
-    console.log("File updated. Updated dashboard.")
-    console.log(infoArray)
-    io.sockets.emit('infoArray',infoArray)
-        // dashboardSockets.broadcast(infoArray)
+dashboardSockets.on('connection', function(socket){
+    broadcastVideos();
+});
+
+function broadcastVideos(){
+    sessionStore.all((err, sessions)=>{
+        // Only return user sessions that have a video 
+        sessions = sessions.filter(session => {
+            return !!session.latestVideoPath;
+        });
+        console.log('broadcastVideos sessions', sessions);
+        dashboardSockets.emit('users', sessions);
+    }); 
 }
--- dist/js/dashboard.js
+++ dist/js/dashboard.js
@@ -1,12 +1,13 @@
-const socket = io()
-socket.on('infoArray', data => {
-  console.log(data)
+const socket = io('/dashboard');
+
+socket.on('users', data => {
+  console.log('users', data)
   data.forEach( user => {
     let thisVideoBox = document.createElement('div')
     let thisVideo = document.createElement('video')
     let thisLabel = document.createElement('h3')
     thisLabel.textContent = user.username
-    thisVideo.setAttribute('src', user.path)
+    thisVideo.setAttribute('src', '/' + user.latestVideoPath)
     thisVideo.setAttribute('autoplay', true)
     thisVideo.setAttribute('loop', true)
     thisVideoBox.appendChild(thisLabel)
