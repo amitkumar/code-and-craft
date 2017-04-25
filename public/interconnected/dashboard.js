b4b5f0fa8871f83fbbfee3ff9e4f4fab63fba6ae
Amit Kumar <email@amitkumar.com>
2017-04-25 18:08:06 -0400

    fixing dashboard display to use chain as data source

--- public/interconnected/dashboard.js
+++ public/interconnected/dashboard.js
@@ -3,38 +3,40 @@
     var database = firebase.database();
     var storage = firebase.storage();
     var amOnline = database.ref('.info/connected');
-    var presenseRef = database.ref('interconnected/presence/');
+    // var presenseRef = database.ref('interconnected/presence/');
+    var chainRef = database.ref('interconnected/chain/');
     var latestCommitsRef = database.ref('interconnected/latest-by-user/');
 
     var repeatBlock = 1;
-    var latestCommits = [];
+    var latestCommitsByUser = {};
+    var chain;
 
     firebase.auth().onAuthStateChanged(function(user) {
         console.log('auth', user);
         if (user) {
-            var presentUsersIds = [];
-
-            presenseRef.on('value', function(snapshot) {
-                var presentUsers = snapshot.val();
-                console.log('presense', presentUsers);
-                if (presentUsers){
-                    presentUsersIds = Object.keys(presentUsers);
-                    console.log('presentUsers', presentUsersIds);    
-                }
-            });
-
+            
             latestCommitsRef.on('value', function(snapshot) {
                 latestCommits = snapshot.val();
                 console.log('latestCommits', latestCommits);
-                redraw();
+                
+            });
+
+            chainRef.on('value', function(snapshot) {
+                chain = snapshot.val();
+                console.log('chain', chain);
+                if (chain && latestCommitsByUser){
+                    redraw();
+                }
             });
 
             function redraw(){
                 var $grid = $('#grid');
 
                 $grid.empty();
-                presentUsersIds.forEach(function(userId, index) {
-                    var commit = latestCommits[userId];
+                Object.keys(chain).forEach(function(chainKey, index) {
+                    var chainItem = chain[chainKey];
+                    var commit = latestCommitsByUser[chainItem.uid];
+                    console.log('chainItem', chainItem);
                     if (commit) {
                         console.log('commit', commit)
         
